0434fa2119591db5c311440606d2fb6e254f998b
Wendall Hsu <Wendall@Wendalls-MacBook-Pro.local>
2016-05-15 21:48:13 -0400

    Added routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,143 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// GET posts
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts) {
+    if (err) {
+      return next(err);
+    }
+    
+    res.json(posts);
+  });
+});
+
+// POST new post
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+  
+  post.save(function(err, post) {
+    if (err) {
+      return next(err);
+    }
+    
+    res.json(post);
+  });
+});
+
+// set parameters
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+  
+  query.exec(function(err, post) {
+    if (err) {
+      return next(err);
+    }
+    
+    if (!post) {
+      return next(new Error("Can't find post"));
+    }
+    
+    req.post = post;
+    return next();
+  });
+});
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+  
+  query.exec(function(err, comment) {
+    if (err) {
+      return next(err);
+    }
+    
+    if (!comment) {
+      return next(new Error("Can't find comment"));
+    }
+    
+    req.comment = comment;
+    return next();
+  });
+});
+
+// GET specific post
+router.get('/posts/:post', function(req, res) {
+  req.post.populate('comments', function(err, post) {
+    if (err) {
+      return next(err);
+    }
+    
+    res.json(req.post);
+  });
+});
+
+// PUT increase post upvote count
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if (err) {
+      return next(err);
+    }
+    
+    res.json(post);
+  });
+});
+
+
+// GET comments
+router.get('/posts/:post/comments', function(req, res, next) {
+  Comment.find(function(err, comments) {
+    if (err) {
+      return next(err);
+    }
+    
+    res.json(comments);
+  });
+});
+
+// POST new comment
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+  
+  comment.save(function(err, comment) {
+    if (err) {
+      return next(err);
+    }
+    
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if (err) {
+        return next(err);
+      }
+      
+      res.json(comment);
+    });
+  });
+});
+
+// GET specific comment
+router.get('/posts/:post/comments/:comment', function(req, res) {    
+    res.json(req.comment);
+});
+
+// PUT increase comment upvote count
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment) {
+    if (err) {
+      return next(err);
+    }
+    
+    res.json(comment);
+  });
 });
 
 module.exports = router;
